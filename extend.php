683eda634345cf415f3d90a225aa1f86a66b480e
Kevo <customworldcompany@gmail.com>
2021-01-26 14:36:56 +0100

    Beta 15 update - core.yml

--- extend.php
+++ extend.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of Flarum.
+ * This file is part of Flarum LOLCat Language Pack.
  *
  * For detailed copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
